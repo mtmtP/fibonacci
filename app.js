38ee6ea61613cd993a8f9e0ba63330aab4a5d7c0
mtmtP <matsumoto0916@gmail.com>
2020-09-08 20:09:53 +0900

    memo化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 'use strict';
-function fib(n){
-   if(n===0){
-       return 0;
-   }else if (n === 1){
-       return 1;
-   }else {
-       return fib(n-1)+fib(n-2);
-   }
+// function fib(n){
+//    if(n===0){
+//        return 0;
+//    }else if (n === 1){
+//        return 1;
+//    }else {
+//        return fib(n-1)+fib(n-2);
+//    }
+// }
+
+// for( let i = 0; i <= 40 ; i++){
+//     console.log(`${i}のフィボナッチ数は${fib(i)}です`)
+// }
+
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fib(n) {
+    if (memo.has(n)) {
+        return memo.get(n)
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(`${i}:${fib(i)}`);
 }
 
-for( let i = 0; i <= 40 ; i++){
-    console.log(`${i}のフィボナッチ数は${fib(i)}です`)
-}
